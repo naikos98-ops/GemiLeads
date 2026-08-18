777d34af753c938c43fd4283fbf23c8b45048eaa
Nikos Karampoikis <naikos98@hotmail.com>
2026-08-18 22:34:06 +0300

    fix: correct default sender email to match brevo verified domain

--- config/settings.py
+++ config/settings.py
@@ -93,8 +93,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() == "true"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Gemi Leads <notifications@gemileads.gr>")
-EMAIL_REPLY_TO = os.getenv("EMAIL_REPLY_TO", "support@gemileads.gr")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Gemi Leads <notifications@send.gemileads.gr>")
+EMAIL_REPLY_TO = os.getenv("EMAIL_REPLY_TO", "info@gemileads.gr")
 
 # Stripe settings
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
